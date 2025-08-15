aa507580b79bafee57ba9aafed2ac0827caba6e1
simuversestudio <simuversestudio@gmail.com>
2025-08-16 00:07:25 +0900

    Add files via upload

---
+++ UIUX/js/ex)2-1.js
@@ -0,0 +1,43 @@
+function fact(n){
+    if (n<=1) return n;
+    return n*fact(n-1)
+}
+for (var i=1; i<10;i++){
+    console.log(i+"! = " +fact(i));
+}
+console.log("--------------------------------------------------------------");
+console.log("Hello World");
+console.log("--------------------------------------------------------------");
+var n = 2;
+var n = -2;
+if (n>0)
+    console.log(n + " is positive.")
+else(n>0)
+    console.log(n + " is negative.")
+console.log("--------------------------------------------------------------");
+console.log("Hello World");
+console.log("--------------------------------------------------------------");
+function fact (n) {
+     if (n<=1) return n;
+     return n*fact(n-1);
+}
+for (var i =1; i<10;i++){
+    console.log(i+"! = " +fact(i));
+}
+console.log("--------------------------------------------------------------");
+console.log("Hello World");
+console.log("--------------------------------------------------------------");
+
+function fact(n) {
+    if (n <= 1) return 1;
+    return n * fact(n - 1);
+}
+
+for (var i = 1; i <= 10; i++) {
+    console.log(i + " ! = " + fact(i)); // 콘솔 출력
+    //document.write(i + "! = " + fact(i) + "<br/>"); // HTML 출력
+}
+console.log("--------------------------------------------------------------");
+console.log("Hello World");
+console.log("--------------------------------------------------------------");
+
